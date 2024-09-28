182c460c75daa782c4f22c6055fd86cc7b4b599d
infoaabir <mohamedaabir03@gmail.com>
2024-09-28 19:27:20 +0530

    upd

--- login_pro.php
+++ login_pro.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="res/css/styles.css">
-</head>
-<body>
+
 <?php
 include("config.php");
 
@@ -39,7 +32,4 @@ header("Location: login.php");
 exit();
 ?>
 
-
-</body>
-</html>
 
