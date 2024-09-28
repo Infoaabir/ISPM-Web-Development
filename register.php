1e1149b0e4dc95838cd6e53068a7cae862eb4d2d
infoaabir <mohamedaabir03@gmail.com>
2024-09-28 19:29:08 +0530

    upd

--- register.php
+++ register.php
@@ -1,5 +1,20 @@
 <!DOCTYPE html>
 <html>
+<link rel="stylesheet" href="res/css/styles.css">
+ <!--#include file="header.html" -->
+ <script
+    src="https://code.jquery.com/jquery-3.3.1.js"
+    integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+    crossorigin="anonymous">
+</script>
+
+<script> 
+$(function(){
+  $("#header").load("header.html"); 
+  $("#footer").load("footer.html"); 
+});
+</script> 
+
 <?php
 include("config.php");
 
@@ -34,5 +49,9 @@ if ($result)
 }
 $con->close();
 ?>
+
+ <!--#include file="footer.html" -->
+
+ <div id="footer"></div>
 </body>
 </html>
